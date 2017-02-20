d2dd5e8dcca433f7d708053ee997577efe07e647
Arun M <arunsubmail@gmail.com>
2017-02-20 22:10:26 +0530

    Added common js

---
+++ veltrade/js/vel.js
@@ -0,0 +1,11 @@
+function animateToTop(e) {
+    e.preventDefault();
+    var scrollToTop = window.setInterval(function() {
+        var pos = window.pageYOffset;
+        if ( pos > 0 ) {
+            window.scrollTo( 0, pos - 20 );
+        } else {
+            window.clearInterval( scrollToTop );
+        }
+    }, 16);
+}

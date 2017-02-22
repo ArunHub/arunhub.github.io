fcf4547083055b91e3d12b08cfbc9ecbd11dd17e
Arun M <arunsubmail@gmail.com>
2017-02-22 22:37:16 +0530

    scrolling updated

--- veltrade/js/vel.js
+++ veltrade/js/vel.js
@@ -1,8 +1,10 @@
 function animateToTop(event) {
     event.preventDefault();
+    // bullbear.style.backgroundPosition = "6px -41px";
     var scrollToTop = window.setInterval(function() {
         var pos = window.pageYOffset;
         if ( pos > 0 ) {
+            
             window.scrollTo( 0, pos - 20 );
         } else {
             window.clearInterval( scrollToTop );
@@ -10,13 +12,27 @@ function animateToTop(event) {
     }, 16);
 }
 
+function scrollUp(){
+	bullbear.style.backgroundPosition = "6px -41px";
+}
+
 (function () {
 	window.onscroll = function(){
-		var scrollPos = window.pageYOffset;
-		if (scrollPos > 200) bullbear.style.display = "block"		
-			else
-				bullbear.style.display = "block"
-			
+		var scrollPos = window.pageYOffset,
+		heightPage    = document.body.offsetHeight,
+		touchBottom	  = heightPage/2;
+		if (scrollPos > 200) {
+			console.log(scrollPos,document.body.offsetHeight);
+			//while going down
+			bullbear.style.transform = "scale(1)";	
+		}
+		// if(scrollPos > heightPage/2){
+		// 	scrollUp();
+		// }
+		//while going up less than 200
+			else{				
+				bullbear.style.transform = "scale(0)";		
+			}
 		
 	}
 })();

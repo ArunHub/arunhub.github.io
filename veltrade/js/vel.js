829fdbdce61e22d18785249fd5c3dd648281f69e
Arun <arunsubmail@gmail.com>
2017-05-06 22:04:55 +0530

    menu updated

--- veltrade/js/vel.js
+++ veltrade/js/vel.js
@@ -1,48 +1,40 @@
 function animateToTop(event) {
     event.preventDefault();
-    // bullbear.style.backgroundPosition = "6px -41px";
     var scrollToTop = window.setInterval(function() {
-        var pos = window.pageYOffset;
-        if ( pos > 0 ) {
-            
-            window.scrollTo( 0, pos - 20 );
+        var position = window.pageYOffset;
+        if (position > 0) {
+
+            window.scrollTo(0, position - 20);
         } else {
-            window.clearInterval( scrollToTop );
+            window.clearInterval(scrollToTop);
         }
     }, 16);
 }
 
-function scrollUp(){
-	bullbear.style.backgroundPosition = "6px -41px";
-}
+(function() {
+    var previousScroll = 0;
 
-(function () {
-	var previousScroll = 0;
-	window.onscroll = function(){
-		
-		var currentScroll = this.pageYOffset;
+    window.onscroll = function() {
+        var currentScroll = this.pageYOffset;
 
-			//while going down
-		if (currentScroll > previousScroll) {
-			console.log('down',currentScroll ,previousScroll)
-			bullbear.style.backgroundPosition = "6px 6px";
-			if (currentScroll > 200) {
-				bullbear.style.transform = "scale(1)";
-			} 	
-		}
-		else{				
-			console.log('up',currentScroll ,previousScroll);
-			bullbear.style.backgroundPosition = "6px -41px";
-			if (currentScroll < 200) {
-				bullbear.style.transform = "scale(0)";
-			} 	
-			
-		}
-		previousScroll = currentScroll;
-		
-	}
+        //while going down
+        if (currentScroll > previousScroll) {
+            bullbear.style.backgroundPosition = "6px 6px";
+            if (currentScroll > 200) {
+                bullbear.style.transform = "scale(1)";
+            }
+        } else {
+            bullbear.style.backgroundPosition = "6px -41px";
+            if (currentScroll < 200) {
+                bullbear.style.transform = "scale(0)";
+            }
+
+        }
+        previousScroll = currentScroll;
+    }
+    
 })();
 
 var bullbear = document.getElementById('bullbear');
 
-bullbear.addEventListener('click',animateToTop,false);
+bullbear.addEventListener('click', animateToTop, false);

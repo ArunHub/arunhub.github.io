04322e62aeaa1647f2b661767cbf422600c2c13e
Arun <arunsubmail@gmail.com>
2017-05-17 11:52:29 +0530

    svg animation added

--- veltrade/js/vel.js
+++ veltrade/js/vel.js
@@ -1,3 +1,4 @@
+// Auto initialize function for scroll to top
 function animateToTop(event) {
     event.preventDefault();
     var scrollToTop = window.setInterval(function() {
@@ -11,25 +12,27 @@ function animateToTop(event) {
     }, 8);
 }
 
+
+// Auto initialize function for scroll position
 (function() {
     var previousScroll = 0;
 
     window.onscroll = function() {
         var currentScroll = this.pageYOffset,
-      	SCROLL_AFTER_POS	= 200;
+            SCROLL_AFTER_POS = 200;
 
         //while going down
         if (currentScroll > previousScroll) {
-            bullbear.style.backgroundPosition = '2px -1px';            
+            bullbear.style.backgroundPosition = '2px -1px';
             if (currentScroll > SCROLL_AFTER_POS) {
                 bullbear.style.transform = 'scale(1)';
-            		bullbear.style.display = 'block';
+                bullbear.style.display = 'block';
             }
         } else {
             bullbear.style.backgroundPosition = '2px -58px';
             if (currentScroll < SCROLL_AFTER_POS) {
                 bullbear.style.transform = 'scale(0)';
-            	  bullbear.style.display = 'none';            	  
+                bullbear.style.display = 'none';
             }
         }
 
@@ -39,5 +42,12 @@ function animateToTop(event) {
 })();
 
 var bullbear = document.getElementById('bullbear');
-
 bullbear.addEventListener('click', animateToTop, false);
+
+window.onload = function() {
+
+    document.getElementById('bestway').style.transform = "translateY(0px)";
+    document.getElementById('bestway').style.opacity = "1";
+    document.getElementById('learn-forex').style.transform = "scale(1)";
+
+};
